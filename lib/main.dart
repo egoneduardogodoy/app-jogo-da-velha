a81e26021497d398e598151068521d42f5196e9b
egon.edu <egon.edu@gmail.com>
2025-01-27 19:52:40 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false, // Desativar modo debug
+    home: Scaffold(
+      body: Center(
+        child: JogoDaVelha(),
+      ),
+    ),
+  ));
+}
+
+class JogoDaVelha extends StatefulWidget {
+  const JogoDaVelha({super.key});
+
+  @override
+  State<JogoDaVelha> createState() => _JogoDaVelhaState();
+}
+
+class _JogoDaVelhaState extends State<JogoDaVelha> {
+  String _jogador = 'X';
+  List<String> _tabuleiro = List.filled(9, '');
+  bool _contraMaquina = false;
+  final Random _randomico = Random();
+
+  void _iniciarJogo() {
+    setState(() {
+      _tabuleiro = List.filled(9, '');
+      _jogador = 'X';
+    });
+  }
+
+  void _trocaJogador() {
+    setState(() {
+      _jogador = _jogador == 'X' ? 'O' : 'X';
+    });
+  }
+
+  void _mostreDialogoVencedor(String jogador) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(jogador == 'Empate' ? 'Empate!' : 'Vencedor: $jogador'),
+          actions: [
+            ElevatedButton(
+              child: const Text('Reiniciar Jogo'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _iniciarJogo();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  bool _verificarVencedor(String jogador) {
+    const posicoesVencedoras = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (var posicoes in posicoesVencedoras) {
+      if (_tabuleiro[posicoes[0]] == jogador &&
+          _tabuleiro[posicoes[1]] == jogador &&
+          _tabuleiro[posicoes[2]] == jogador) {
+        _mostreDialogoVencedor(jogador);
+        return true;
+      }
+    }
+
+    if (!_tabuleiro.contains('')) {
+      _mostreDialogoVencedor('Empate');
+      return true;
+    }
+    return false;
+  }
+
+  void _jogadaComputador() {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      int movimento;
+      do {
+        movimento = _randomico.nextInt(9);
+      } while (_tabuleiro[movimento] != '');
+      setState(() {
+        _tabuleiro[movimento] = 'O';
+        if (!_verificarVencedor('O')) {
+          _trocaJogador();
+        }
+      });
+    });
+  }
+
+  void _jogada(int index) {
+    if (_tabuleiro[index] == '') {
+      setState(() {
+        _tabuleiro[index] = _jogador;
+        if (!_verificarVencedor(_jogador)) {
+          _trocaJogador();
+          if (_contraMaquina && _jogador == 'O') {
+            _jogadaComputador();
+          }
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double altura = MediaQuery.of(context).size.height * 0.5;
+    return Column(
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              Transform.scale(
+                scale: 0.6,
+                child: Switch(
+                  value: _contraMaquina,
+                  onChanged: (value) {
+                    setState(() {
+                      _contraMaquina = value;
+                      _iniciarJogo();
+                    });
+                  },
+                ),
+              ),
+              Text(_contraMaquina ? 'Contra Computador' : 'Contra Humano'),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 10,
+          child: SizedBox(
+            height: altura,
+            width: altura,
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 5.0,
+                mainAxisSpacing: 5.0,
+              ),
+              itemCount: 9,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () => _jogada(index),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(186, 223, 219, 219),
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Center(
+                      child: Text(
+                        _tabuleiro[index],
+                        style: const TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: ElevatedButton(
+            onPressed: _iniciarJogo,
+            child: const Text('Reiniciar Jogo'),
+          ),
+        ),
+      ],
+    );
+  }
+}
